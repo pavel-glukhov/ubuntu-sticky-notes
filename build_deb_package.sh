fb482e5f70c44b42aade693c398c4872dc9f386c
Pavel Glukhov <glukhov.p@gmail.com>
2025-09-15 22:33:28 +0500

    Updated build deb package script.

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -14,27 +14,18 @@ USR_BIN_DIR="$PACKAGE_DIR/usr/local/bin"
 USR_SHARE_DIR="$PACKAGE_DIR/usr/share"
 APPLICATIONS_DIR="$USR_SHARE_DIR/applications"
 APP_DIR="$USR_SHARE_DIR/ubuntu-sticky-notes"
-
+METAINFO_DIR="$USR_SHARE_DIR/metainfo"
 APP_INFO_JSON="ubuntu-sticky-notes/app_info.json"
 APP_EXEC="ubuntu-sticky-notes"
-APP_ICON="/usr/share/ubuntu-sticky-notes/resources/icons/app.png"
 APP_MAIN_SCRIPT="/usr/share/ubuntu-sticky-notes/main.py"
+ICON_PATH="/usr/share/ubuntu-sticky-notes/resources/icons/app.png"
 
 # ----------------------
 # Create necessary directories if they don't exist
 # ----------------------
-for dir in "$DEBIAN_DIR" "$USR_BIN_DIR" "$APPLICATIONS_DIR" "$APP_DIR"; do
+for dir in "$DEBIAN_DIR" "$USR_BIN_DIR" "$APPLICATIONS_DIR" "$APP_DIR" "$METAINFO_DIR"; do
     [ ! -d "$dir" ] && mkdir -p "$dir"
 done
-
-# ----------------------
-# Create executable script in /usr/local/bin
-# ----------------------
-cat > "$USR_BIN_DIR/$APP_EXEC" <<EOL
-#!/bin/bash
-python3 "$APP_MAIN_SCRIPT"
-EOL
-chmod +x "$USR_BIN_DIR/$APP_EXEC"
 
 # ----------------------
 # Copy all application files to /usr/share/ubuntu-sticky-notes
@@ -48,9 +39,22 @@ SERVICE_NAME=$(jq -r '.service_name' "$APP_INFO_JSON")
 APP_NAME=$(jq -r '.app_name' "$APP_INFO_JSON")
 VERSION=$(jq -r '.version' "$APP_INFO_JSON")
 DESCRIPTION=$(jq -r '.description' "$APP_INFO_JSON")
-MAINTAINER="$(jq -r '.author' "$APP_INFO_JSON") <$(jq -r '.email' "$APP_INFO_JSON")>"
+AUTHOR=$(jq -r '.author' "$APP_INFO_JSON")
+EMAIL=$(jq -r '.email' "$APP_INFO_JSON")
+MAINTAINER="$AUTHOR <$EMAIL>"
 ARCHITECTURE=$(jq -r '.architecture' "$APP_INFO_JSON")
 DEPENDENCIES=$(jq -r '.dependencies | join(", ")' "$APP_INFO_JSON")
+WEBSITE=$(jq -r '.website' "$APP_INFO_JSON")
+LICENSE=$(jq -r '.license' "$APP_INFO_JSON")
+
+# ----------------------
+# Create executable script in /usr/local/bin
+# ----------------------
+cat > "$USR_BIN_DIR/$APP_EXEC" <<EOL
+#!/bin/bash
+python3 "$APP_MAIN_SCRIPT"
+EOL
+chmod +x "$USR_BIN_DIR/$APP_EXEC"
 
 # ----------------------
 # Create DEBIAN/control file
@@ -63,6 +67,8 @@ Priority: optional
 Architecture: $ARCHITECTURE
 Depends: $DEPENDENCIES
 Maintainer: $MAINTAINER
+Vendor: $AUTHOR
+Homepage: $WEBSITE
 Description: $DESCRIPTION
 EOL
 
@@ -87,12 +93,31 @@ cat > "$DESKTOP_FILE" <<EOL
 Name=$APP_NAME
 Comment=$DESCRIPTION
 Exec=/usr/local/bin/$APP_EXEC
-Icon=$APP_ICON
+Icon=$ICON_PATH
 Terminal=false
 Type=Application
 Categories=Utility;
 StartupNotify=true
-StartupWMClass=APP_NAME
+StartupWMClass=$APP_NAME
+EOL
+
+# ----------------------
+# Create AppStream metainfo file
+# ----------------------
+METAINFO_FILE="$METAINFO_DIR/$SERVICE_NAME.metainfo.xml"
+cat > "$METAINFO_FILE" <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<component type="desktop-application">
+  <id>$APP_EXEC.desktop</id>
+  <name>$APP_NAME</name>
+  <summary>$DESCRIPTION</summary>
+  <developer_name>$AUTHOR</developer_name>
+  <description>
+    <p>$DESCRIPTION</p>
+  </description>
+  <url type="homepage">$WEBSITE</url>
+  <project_license>$LICENSE</project_license>
+</component>
 EOL
 
 # ----------------------
